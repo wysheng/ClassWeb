7d67d0950b176e3a136890b57e31a1fd668ca931
helencoder <731873664@qq.com>
2017-08-16 17:27:16 +0800

    模型操作类

---
+++ src/main/java/model/News.java
@@ -0,0 +1,63 @@
+package model;
+
+/**
+ * 资讯类
+ *
+ * Created by helencoder on 2017/8/16.
+ */
+public class News {
+    private String id;
+    private String link;
+    private String title;
+    private String type;
+    private String tags;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
