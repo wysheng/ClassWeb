6bd11dd6faef57ed9e822ad2fbc2e7c1f1484e32
helencoder <731873664@qq.com>
2017-09-21 16:38:08 +0800

    中文字符转换拼音及拼接方法

--- src/main/java/com/helencoder/filter/FilterController.java
+++ src/main/java/com/helencoder/filter/FilterController.java
@@ -1,13 +1,14 @@
 package com.helencoder.filter;
 
+import com.helencoder.util.BasicUtil;
+import com.helencoder.util.TransformUtil;
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * 文本过滤检测控制器(DFA)
@@ -18,8 +19,19 @@ public class FilterController {
     public static void main(String[] args) {
         // 测试用例
 
-        String text = "";
+        String text = "我爱你";
+        //sensitiveWordsTransform(text);
 
+        List<String> characterList = combineCharacter("我爱你", "wo ai ni");
+        for (String character : characterList) {
+            System.out.println(character);
+        }
+
+        // java8聚合操作
+//        List nums = Arrays.asList(1, 3, null, 8, 7, 8, 13, 10);
+//        nums.stream().filter(num -> num != null).distinct().forEach(System.out::println);
+//        int value = Stream.of(1, 2, 3, 4).reduce(100, (sum, item) -> sum + item);
+//        System.out.println(value);
 
     }
 
@@ -62,6 +74,69 @@ public class FilterController {
 
         return wordsList;
     }
+
+    /**
+     * 敏感词拼音转换、拼接(纯中文)
+     */
+    public static List<String> sensitiveWordsTransform(String word) {
+        List<String> wordsList = new ArrayList<String>();
+        // 进行全中文字符检测
+        boolean flag = BasicUtil.isContainsOtherCharacter(word);
+        if (!flag) {
+            String pinyinStr = TransformUtil.transformToPinyin(word);
+
+            System.out.println(pinyinStr);
+        } else {
+            System.out.println("非全中文字符，未进行处理");
+            wordsList.add(word);
+        }
+
+        return wordsList;
+    }
+
+    /**
+     * 中文字符及期对应拼音组合转换(添加首字母)
+     */
+    public static List<String> combineCharacter(String word, String pinyin) {
+        List<String> characterList = new ArrayList<String>();
+
+        String[] pinyinArr = pinyin.split(" ");
+        String[] wordArr = new String[pinyinArr.length];
+        StringBuilder pinyinStr = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            wordArr[i] = word.charAt(i) + "";
+            pinyinStr.append(pinyinArr[i]);
+        }
+
+        characterList.add(word);
+        characterList.add(pinyinStr.toString());
+
+        int length = wordArr.length;
+        for (int i = 0; i < length; i++) {
+            // 中文开头
+            StringBuilder wordSB = new StringBuilder();
+            // 拼音开头
+            StringBuilder pinyinSB = new StringBuilder();
+            // 确立当前的index
+            for (int j = 0; j < length; j++) {
+                if (j == i) {
+                    wordSB.append(pinyinArr[j]);
+                    pinyinSB.append(wordArr[j]);
+                } else {
+                    wordSB.append(wordArr[j]);
+                    pinyinSB.append(pinyinArr[j]);
+                }
+            }
+            characterList.add(wordSB.toString());
+            characterList.add(pinyinSB.toString());
+        }
+
+        return characterList;
+    }
+
+    /**
+     * 字符串数组shuffle拼接
+     */
 
 
 
